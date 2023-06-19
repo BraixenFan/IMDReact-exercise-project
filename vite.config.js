b8efa416a94b6cb7fe35025a016b0a32d2ff0c80
Carlos Plata <47064395+BraixenFan@users.noreply.github.com>
2023-06-19 14:52:18 -0600

    Delete deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/IMDReact-exercise-project/"
+  base: "/IMDReact-exercise-project/",
+  plugins: [react()]
 });
